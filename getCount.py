5d40298baed68947cdfb430aa51d3be0af89460e
Ahmad <ahmad.melegy@gmail.com>
2014-08-26 23:09:41 +0300

    Intial Commit

--- getCount.py
+++ getCount.py
@@ -1,35 +1,36 @@
 import urllib2
 import time
 import pyglet
-import sys
 
-
-def find_between(s, first, last ):
+def getFansNumber(page, first, last):
     try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
-        return s[start:end]
+        start = page.index(first) + len(first)
+        end = page.index(last,start)
+        return page[start:end]
     except ValueError:
-        return "Error"
+        print "Error Occured !"
+
+def exiter(length):
+	pyglet.app.exit()
+	exit()
 
 while(1):
-	req = urllib2.Request('https://www.facebook.com/CATReloaded')
-	response = urllib2.urlopen(req)
-	the_page = response.read()
-
-	num = find_between(the_page,'<span class="_52id _50f5 _50f7">','<span')
-
-	if(num >= '2000'):
-		print "Yes"
-		import pyglet
-		foo=pyglet.media.load("beep.wav")
-		foo.play()
-		def exiter(dt):
-			pyglet.app.exit()
-		pyglet.clock.schedule_once(exiter, foo.duration)
-		pyglet.app.run()
-		break
+	try:
+		request = urllib2.Request('https://www.facebook.com/CATReloaded')
+		response = urllib2.urlopen(request)
+		web_page = response.read()
+	except Exception:
+		print "Error, Check your connection !"
 		exit()
+
+	fansNumber = getFansNumber(web_page,'<span class="_52id _50f5 _50f7">','<span')
+
+	if(fansNumber >= '2,000'):
+		print "Mission Accompilished !"
+		sound = pyglet.media.load("beep.wav")
+		sound.play()
+		pyglet.clock.schedule_once(exiter, sound.duration)
+		pyglet.app.run()
+
 	else:
 		time.sleep(60)
-		
